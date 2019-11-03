d2436b105c6ac92d85071605b3e04571499fb1af
Wilfredo <casas.farach@gmail.com>
2019-11-03 22:49:23 +0100

    improved copy for converting users to premium

--- src/app.jsx
+++ src/app.jsx
@@ -3,39 +3,61 @@ import ReactDOM from "react-dom";
 import { Router } from "./utils/router";
 import { sessionInit, isLogged } from "./utils/sessions";
 
-
-const JobsController = React.lazy(_=>import("./pages/Jobs/JobsController"));
-const LoginController = React.lazy(_=>import("./pages/Login/LoginController"));
-const PrePayJobController = React.lazy(_=>import("./pages/PrePayJob/PrePayJobController"));
-const PremiumBuyController = React.lazy(_=>import("./pages/PremiumBuy/PremiumBuyController"));
-const PremiumSetController = React.lazy(_=>import("./pages/PremiumSet/PremiumSetController"));
-const JobConfirmController = React.lazy(_=>import("./pages/JobConfirm/JobConfirmController"));
-const PersonFormController = React.lazy(_=>import("./pages/PersonForm/PersonFormController"));
-const JobFormController = React.lazy(_=>import("./pages/JobForm/JobFormController"));
-const PrePayPersonController = React.lazy(_=>import("./pages/PrePayPerson/PrePayPersonController"));
-const PostTypeController = React.lazy(_=>import("./pages/PostType/PostTypeController"));
-const PersonConfirmController = React.lazy(_=>import("./pages/PersonConfirm/PersonConfirmController"));
+const JobsController = React.lazy(_ => import("./pages/Jobs/JobsController"));
+const LoginController = React.lazy(_ =>
+    import("./pages/Login/LoginController")
+);
+const PrePayJobController = React.lazy(_ =>
+    import("./pages/PrePayJob/PrePayJobController")
+);
+const PremiumBuyController = React.lazy(_ =>
+    import("./pages/PremiumBuy/PremiumBuyController")
+);
+const PremiumSetController = React.lazy(_ =>
+    import("./pages/PremiumSet/PremiumSetController")
+);
+const JobConfirmController = React.lazy(_ =>
+    import("./pages/JobConfirm/JobConfirmController")
+);
+const PersonFormController = React.lazy(_ =>
+    import("./pages/PersonForm/PersonFormController")
+);
+const JobFormController = React.lazy(_ =>
+    import("./pages/JobForm/JobFormController")
+);
+const PrePayPersonController = React.lazy(_ =>
+    import("./pages/PrePayPerson/PrePayPersonController")
+);
+const PostTypeController = React.lazy(_ =>
+    import("./pages/PostType/PostTypeController")
+);
+const PersonConfirmController = React.lazy(_ =>
+    import("./pages/PersonConfirm/PersonConfirmController")
+);
 
 //console.log(sessionInit());
 
 let routes = [
-    {path:"", component:JobsController},
-    {path:"login", component:LoginController},
-    {path:"prePayJob",component:PrePayJobController},
-    {path:"premiumBuy",component:PremiumBuyController,guard:[isLogged]},
-    {path:"premiumSet",component:PremiumSetController,guard:[isLogged]},
-    {path:"JobConfirm",component:JobConfirmController},
-    {path:"JobConfirm/:id",component:JobConfirmController},
-    {path:"personForm",component:PersonFormController},
-    {path:"jobForm",component:JobFormController},
-    {path:"PrePayPerson",component:PrePayPersonController},
-    {path:"postType",component:PostTypeController},
-    {path:"personConfirm",component:PersonConfirmController},
-    {path:"personConfirm/:id",component:PersonConfirmController},
+    { path: "", component: JobsController },
+    { path: "login", component: LoginController },
+    { path: "prePayJob", component: PrePayJobController },
+    { path: "premiumBuy", component: PremiumBuyController, guard: [isLogged] },
+    { path: "premiumSet", component: PremiumSetController, guard: [isLogged] },
+    { path: "JobConfirm", component: JobConfirmController },
+    { path: "JobConfirm/:id", component: JobConfirmController },
+    { path: "personForm", component: PersonFormController },
+    { path: "jobForm", component: JobFormController },
+    { path: "PrePayPerson", component: PrePayPersonController },
+    { path: "postType", component: PostTypeController },
+    { path: "personConfirm", component: PersonConfirmController },
+    { path: "personConfirm/:id", component: PersonConfirmController }
 ];
-async function init(){
+async function init() {
     await sessionInit();
-    ReactDOM.render(<Router routes={routes} guardFalse={LoginController}   />, document.querySelector("#app"));
+    ReactDOM.render(
+        <Router routes={routes} guardFalse={LoginController} />,
+        document.querySelector("#app")
+    );
 }
 
 init();
--- src/pages/PremiumBuy/PremiumBuyController.jsx
+++ src/pages/PremiumBuy/PremiumBuyController.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import StripeButton3 from "../../components/StripeButton3/StripeButton3";
 import { LanguageContext } from "../../components/Language/LanguageContext";
 import { BodyComponent } from "../../components/Body/BodyComponent";
+import { Link } from "react-router-dom";
 
 export default class PremiumBuy extends React.Component {
-console.log("dogs", this.props)
-
-
-
     render() {
         return (
             <BodyComponent
@@ -16,13 +13,11 @@ console.log("dogs", this.props)
                 <div className="container">
                     <p className="text">
                         {" "}
-                        Las cuentas premium cuestan tan solo 2 dolares al mes y
-                        le permiten ver todos los anuncios de trabajo
+                        Las cuentas premium cuestan tan solo 2 dolares por
+                        semana y le permiten ver todos los anuncios de trabajo.
                         <br />
                         <br />
-                        Puede cancelar su cuenta premium cuando quiera. Si
-                        encuentra trabajo despues de 2 semanas habra invertido
-                        tan solo 4 dolares en un nuevo trabajo.
+                        Puede cancelar su cuenta premium cuando quiera.
                         <br />
                         <br />
                     </p>
