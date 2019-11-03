6de72b8bd5007c05edbf7c40edcf42ca1137c2fa
Wilfredo <casas.farach@gmail.com>
2019-11-03 01:14:42 +0100

    .

--- src/pages/PremiumBuy/PremiumBuyController.jsx
+++ src/pages/PremiumBuy/PremiumBuyController.jsx
@@ -3,39 +3,39 @@ import StripeButton3 from "../../components/StripeButton3/StripeButton3";
 import { LanguageContext } from "../../components/Language/LanguageContext";
 import { BodyComponent } from "../../components/Body/BodyComponent";
 
-
 export default class PremiumBuy extends React.Component {
     render() {
         return (
-            <BodyComponent toggleLanguage={this.props.navigation.toggleLanguage}>
-            <div className="container">
-                <p className="text">
-                    {" "}
-                    Las cuentas premium por ahora solo cuestan 5 dolares al mes.
-                    Este precio probablemente subira pronto.
-                    <br />
-                    <br />
-                    Solo las cuentas premium pueden cerrar sus anuncios. Pronto
-                    tambien podran editar sus anuncios y tendran un TOP post
-                    gratuito cada mes.
-                    <br />
-                    <br />
-                    Puede cancelar su cuenta premium cuando desee
-                </p>
-                <div className="PrePayPersonButtons">
-                    <StripeButton3 />
+            <BodyComponent
+                toggleLanguage={this.props.navigation.toggleLanguage}
+            >
+                <div className="container">
+                    <p className="text">
+                        {" "}
+                        Las cuentas premium cuestan tan solo 2 dolares al mes y
+                        le permiten ver todos los anuncios de trabajo
+                        <br />
+                        <br />
+                        Puede cancelar su cuenta premium cuando quiera. Si
+                        encuentra trabajo despues de 2 semanas habra invertido
+                        tan solo 4 dolares en un nuevo trabajo.
+                        <br />
+                        <br />
+                    </p>
+                    <div className="PrePayPersonButtons">
+                        <StripeButton3 />
 
-                    <button
-                        onClick={this.cancelPay}
-                        className="buttonBasic buttonOpaque prePay"
-                    >
-                        {this.context.PrePayPerson.button}
-                    </button>
-                    <div className="contactInfo">
-                        <p>{this.context.contactInfo.contactInfo}</p>
+                        <button
+                            onClick={this.cancelPay}
+                            className="buttonBasic buttonOpaque prePay"
+                        >
+                            {this.context.PrePayPerson.button}
+                        </button>
+                        <div className="contactInfo">
+                            <p>{this.context.contactInfo.contactInfo}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
             </BodyComponent>
         );
     }
