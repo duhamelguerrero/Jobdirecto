d1c46a2972c9cdf4231376ed4d70aa1c7611bcd0
Wilfredo <casas.farach@gmail.com>
2019-11-10 18:24:36 +0100

    fixing firebase login and https redirection

--- src/pages/Jobs/JobsController.jsx
+++ src/pages/Jobs/JobsController.jsx
@@ -287,10 +287,7 @@ export default class JobsController extends React.Component {
                    
 {/* <p id="welcomeText" className="text">Usuarios premium conectados: 36</p> */}
                         {/**/}
-                        <p id="welcomeText" className="text">Encontraron trabajo en lo que va de Noviembre:
-14 personas
-
-</p>
+                       
 
 
                     </div>
